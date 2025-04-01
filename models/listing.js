c68d59d5a145e4fd1599890b4b5289831335ccf4
ritu jha <162335780+ritujha19@users.noreply.github.com>
2025-04-01 15:19:35 +0000

    add listing model

---
+++ models/listing.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description:{
+        type:  String,
+        required: true,
+    },
+    image: {
+        type: String,
+        default: "https://thumbs.cityrealty.com/assets/smart/1004x/webp/1/16/1655f4e3904fb79cb987ab7755d2b3f4b8f37f88/1-city-point.jpg",
+        set: (v) => v === "" ? "https://thumbs.cityrealty.com/assets/smart/1004x/webp/1/16/1655f4e3904fb79cb987ab7755d2b3f4b8f37f88/1-city-point.jpg" : v,
+    },
+    price: Number,
+    location: String,
+    country: String
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
